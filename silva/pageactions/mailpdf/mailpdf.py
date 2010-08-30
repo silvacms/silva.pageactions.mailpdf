ead3040d269e0130cca9d31e11e9d18e6e16d107
Sylvain Viollon <thefunny@gmail.com>
2010-08-30 13:25:00 +0000

    Update form implementation.

--- silva/pageactions/mailpdf/mailpdf.py
+++ silva/pageactions/mailpdf/mailpdf.py
@@ -3,10 +3,10 @@
 # See also LICENSE.txt
 # $Id$
 
-import MimeWriter
-import base64
-import StringIO
 from smtplib import SMTPException
+import MimeWriter
+import StringIO
+import base64
 
 from zope import interface, schema, component
 from zope.i18n import translate
@@ -15,12 +15,13 @@ from zope.traversing.browser import absoluteURL
 
 from Products.Silva import mangle
 from Products.Silva.mail import sendmail
-from silva.core.layout import interfaces
 
 from collective.captcha.form.field import Captcha
-from silva.core.views import forms as silvaforms
-from silva.core.views.interfaces import IHTTPResponseHeaders
+from silva.core.interfaces import ISilvaObject
+from silva.core.layout import interfaces
+from silva.core.views.interfaces import INonCachedLayer
 from silva.pageactions.base.base import PageAction
+from zeam.form import silva as silvaforms
 
 from five import grok
 
@@ -28,32 +29,30 @@ _ = MessageFactory("pageactions")
 
 
 class IMailForm(interface.Interface):
-
     to = schema.TextLine(title=_(u"To"))
     subject = schema.TextLine(title=_(u"Subject"))
     captcha = Captcha(title=_(u"Captcha"), required=True)
 
 
 class MailThatPage(silvaforms.PublicForm):
-
     grok.name('mailthatpage.html')
-    form_fields = grok.Fields(IMailForm)
+    grok.context(ISilvaObject)
+    fields = silvaforms.Fields(IMailForm)
 
     label = _(u"Send this page by mail")
 
-    def update(self):
-        self.HEAD()
+    def __call__(self):
+        interface.alsoProvides(self.request, INonCachedLayer)
+        return super(MailThatPage, self).__call__()
 
-    def HEAD(self):
-        self.response.setHeader(
-            'Cache-Control',
-            'no-cache, must-revalidate, post-check=0, pre-check=0')
-        self.response.setHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT')
-        self.response.setHeader('Pragma', 'no-cache')
+    @silvaforms.action(_(u"Send"))
+    def send(self):
+        errors, data = self.extractData()
+        if errors:
+            return silvaforms.FAILURE
 
-    @grok.action(_(u"Send"))
-    def send(self, to, subject, captcha):
-
+        to = data['to']
+        subject = data['subject']
         metadata = interfaces.IMetadata(self.context.get_root())
         mail_body = metadata('mail-pageactions', 'mail-body')
         mail_from = metadata('mail-pageactions', 'mail-from')
@@ -62,11 +61,6 @@ class MailThatPage(silvaforms.PublicForm):
             self.status = _(u"Mail settings are not configured, "
                             "can't send mail at the moment.")
             return
-
-        # UTF-8 safe
-        to = to.encode('utf-8')
-        subject = subject.encode('utf-8')
-        mail_from = mail_from.encode('utf-8')
 
         url = absoluteURL(self.context, self.request)
         mail_body = mail_body % {'title': self.context.get_title(), 'url': url}
@@ -104,9 +98,8 @@ class MailThatPage(silvaforms.PublicForm):
                 url, message_status=translate(
                     _(u"Your message has been sent."), context=self.request))
             self.redirect(exit_url)
+        return silvaforms.SUCCESS
 
 
 class MailPDFAction(PageAction):
-
     grok.order(40)
-
